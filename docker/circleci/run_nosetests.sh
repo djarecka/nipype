d8d4c74121370c3f97dfa8f4ec6ad64602643907
Dorota Jarecka <djarecka@gmail.com>
2016-11-27 22:38:41 -0500

    testicng circel, changing run_nosetes

--- docker/circleci/run_nosetests.sh
+++ docker/circleci/run_nosetests.sh
@@ -23,15 +23,17 @@ fi
 # Run tests
 cd /root/src/nipype/
 make clean
-nosetests -s nipype -c /root/src/nipype/.noserc --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}.xml"
+pip install -U pytest
+py.test nipype
+#dnosetests -s nipype -c /root/src/nipype/.noserc --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}.xml"
 
 # Workaround: run here the profiler tests in python 3
-if [[ "${PYTHON_VERSION}" -ge "30" ]]; then
-    echo '[execution]' >> /root/.nipype/nipype.cfg
-    echo 'profile_runtime = true' >> /root/.nipype/nipype.cfg
-    nosetests nipype/interfaces/tests/test_runtime_profiler.py --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_profiler.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_profiler.xml"
-    nosetests nipype/pipeline/plugins/tests/test_multiproc*.py --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_multiproc.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_multiproc.xml"
-fi
+#dif [[ "${PYTHON_VERSION}" -ge "30" ]]; then
+#d    echo '[execution]' >> /root/.nipype/nipype.cfg
+#d    echo 'profile_runtime = true' >> /root/.nipype/nipype.cfg
+#d    nosetests nipype/interfaces/tests/test_runtime_profiler.py --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_profiler.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_profiler.xml"
+#d    nosetests nipype/pipeline/plugins/tests/test_multiproc*.py --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_multiproc.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_multiproc.xml"
+#dfi
 
 # Copy crashfiles to scratch
 for i in $(find /root/src/nipype/ -name "crash-*" ); do cp $i /scratch/crashfiles/; done
