693f2b5e0392358647f89aa1329c96a976b17255
Dorota Jarecka <djarecka@gmail.com>
2016-11-29 21:56:11 -0500

    trying to use xml files and flags with codecov

--- docker/circleci/run_nosetests.sh
+++ docker/circleci/run_nosetests.sh
@@ -5,7 +5,7 @@ set -u
 
 PYTHON_VERSION=$( python -c "import sys; print('{}{}'.format(sys.version_info[0], sys.version_info[1]))" )
 
-# Create necessary directories
+# Create necessary directories #czy sratch/pytest tu potrzebny??
 mkdir -p /scratch/pytest /scratch/crashfiles /scratch/logs/py${PYTHON_VERSION}
 
 # Create a nipype config file
@@ -27,15 +27,15 @@ pip install -U pytest
 pip install pytest-raisesregexp
 pip install pytest-cov
 pip install click
-py.test --doctest-modules  --cov=nipype nipype
+py.test --doctest-modules --cov-report xml:/scratch/coverage_py${PYTHON_VERSION}.xml  --cov=nipype nipype
 #dnosetests -s nipype -c /root/src/nipype/.noserc --xunit-file="/scratch/nosetests_py${PYTHON_VERSION}.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}.xml"
 
 # Workaround: run here the profiler tests in python 3
 if [[ "${PYTHON_VERSION}" -ge "30" ]]; then
     echo '[execution]' >> /root/.nipype/nipype.cfg
     echo 'profile_runtime = true' >> /root/.nipype/nipype.cfg
-    py.test  --cov=nipype nipype/interfaces/tests/test_runtime_profiler.py #--xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_profiler.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_profiler.xml"
-    py.test  --cov=nipype nipype/pipeline/plugins/tests/test_multiproc*.py #--xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_multiproc.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_multiproc.xml"
+    py.test --cov-report xml:/scratch/pytest_py${PYTHON_VERSION}_profiler.xml  --cov=nipype nipype/interfaces/tests/test_runtime_profiler.py #--xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_profiler.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_profiler.xml"
+    py.test  --cov-report xml:/scratch/pytest_py${PYTHON_VERSION}_multiproc.xml --cov=nipype nipype/pipeline/plugins/tests/test_multiproc*.py #--xunit-file="/scratch/nosetests_py${PYTHON_VERSION}_multiproc.xml" --cover-xml-file="/scratch/coverage_py${PYTHON_VERSION}_multiproc.xml"
 fi
 
 #teste
